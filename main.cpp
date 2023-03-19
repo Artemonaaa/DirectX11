92c834e646cb858017b0067cf2c5eb3feb18cbf1
Artemona <artemidaabc@gmail.com>
2023-03-19 21:34:52 +0200

    Create the window

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <format>
+
+#include <GLFW/glfw3.h>
+
+auto main() -> int {
+	if (glfwInit() == GLFW_FALSE) {
+		std::cout << "Could not initialize GLFW\n";
+	}
+
+	glfwSetErrorCallback([](int ErrorCode, const char* Description) {
+		std::cout << std::format("GLFW error {}: {}\n", ErrorCode, Description);
+	});
+
+	GLFWwindow* Window = glfwCreateWindow(640, 480, "My 2D game", NULL, NULL);
+	if (Window == nullptr) {
+		std::cout << "Could not create the window\n";
+		glfwTerminate();
+		return 0;
+	}
+
+	while (glfwWindowShouldClose(Window) == GLFW_FALSE) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(Window);
+	glfwTerminate();
+}
